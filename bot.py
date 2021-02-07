3f8306e4785e4df01af6366a4eb17d5c1e4e9d41
timofeev-denis <nakolenke@gmail.com>
2021-02-07 21:24:38 +0300

    Add previous user message to session

--- bot.py
+++ bot.py
@@ -1,24 +1,29 @@
 import requests
 from emoji import emojize
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 
-app = Flask(__name__)
-application = app
+application = Flask(__name__)
+application.config["SECRET_KEY"] = "secret"
 
 
-@app.route("/", methods=["POST"])
+@application.route("/", methods=["POST"])
 def receive_update():
     cake = emojize(":cake:", use_aliases=True)
     pretzel = emojize(":pretzel:")
-    if request.method == "POST":
-        print(request.json)
     chat_id = request.json["message"]["chat"]["id"]
+    if "latest_message" in session:
+        print("Previous message was " + session["latest_message"])
+    else:
+        print("Starting new session")
+    session["latest_message"] = request.json["message"]["text"]
+
     send_message(chat_id, cake + pretzel + " " + request.json["message"]["text"] + "🐧")
     return jsonify(ok=True)
 
 
 def send_message(chat_id, text):
-    token = "1675723385:AAEJxiN5zQMmddupcjcUk7YkRFUWb_TTQlM"
+    with open("bot_token.txt", "r+") as file:
+        token = file.read()
     method = "sendMessage"
     url = f"https://api.telegram.org/bot{token}/{method}"
     data = {"chat_id": chat_id, "text": text}
